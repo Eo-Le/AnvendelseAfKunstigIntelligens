d8ed14c4abb56f569e90ada3fbd83b6faa58cf3a
Dan Nguyen <Eo-Le@Hotmail.com>
2025-01-14 11:02:22 +0000

    add updating site on deployment add scripts.html in _includes/ add layouts from minima theme

---
+++ _posts/2025-01-14-modul-1-introduktion.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title:  "Modul 1: Introduktion"
+date:   2025-01-14 10:27:00 +0000
+categories: eksamen repetition
+permalink: /modul-1-introduktion/
+---
+Repetition af modul 1: Introduktion.
---
+++ assets/js/updateSiteOnDeployment.js
@@ -0,0 +1,19 @@
+console.log("loaded: assets/js/updateSiteOnDeployment.js");
+
+// Fetch the current update timestamp from the meta tag
+const currentUpdate = document.querySelector('meta[name="last-update"]').getAttribute('content');
+
+// Retrieve the previous update timestamp from localStorage. If no key 'lastUpdate' is found then the value will be null
+const previousUpdate = localStorage.getItem('lastUpdate');
+
+// Compare timestamps to detect updates
+if (previousUpdate && previousUpdate !== currentUpdate) {
+    alert(`Hjemmesiden er opdateret!\nGamle version, dato: ${previousUpdate}\nNy version, dato: ${currentUpdate}`);
+
+    // Reload the page if the update is new
+    location.reload();
+} else {
+}
+
+// Store the latest timestamp in localStorage. https://www.w3schools.com/js/js_api_web_storage.asp
+localStorage.setItem('lastUpdate', currentUpdate);
